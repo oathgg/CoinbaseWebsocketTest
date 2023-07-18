f48c56e1744b6b591a4fcaf2c17828e4aae4d0ea
Sander van den Hoed <s.vdhoed@gmail.com>
2023-07-18 09:06:34 +0200

    Removed message parsers and just write response to Console

--- CoinbaseWebsocketTest/Program.cs
+++ CoinbaseWebsocketTest/Program.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 BrokerClientWebSocket ws = new BrokerClientWebSocket("wss://advanced-trade-ws.coinbase.com", OnMessageReceived);
 string msg = GenerateMessage(
-    key: "xxxxx",
+    key: "xxxx",
     secret: "xxxxx",
     productIds: new List<string> { "BTC-USDT", "BTC-USD", "BTC-USDC" },
     channelName: "user",
@@ -20,64 +20,10 @@ Console.ReadKey();
 
 static void OnMessageReceived(string message)
 {
-    var response = JsonConvert.DeserializeObject<StreamResponse>(message);
-    if (response != null)
-    {
-        Console.WriteLine($"OnMessageReceived -> {response.Channel} - {response.Timestamp} - {response.SequenceNum}");
-        switch (response.Channel)
-        {
-            case "ticker_batch":
-            case "ticker":
-                var tr = JsonConvert.DeserializeObject<TickerResponse>(message);
-                if (tr is not null)
-                {
-                    foreach (TickerEvent e in tr.Events)
-                    {
-                        foreach (TickerData t in e.Tickers)
-                        {
-                            Console.WriteLine($"{t.ProductId} - {t.Price}");
-                        }
-                    }
-                }
-                break;
-            case "user":
-                var ur = JsonConvert.DeserializeObject<UserStreamResponse>(message);
-                if (ur is not null)
-                {
-                    foreach (UserEvent e in ur.Events)
-                    {
-                        foreach (CoinbaseOrderStream o in e.Orders)
-                        {
-                            Console.WriteLine("User message received");
-                        }
-                    }
-                }
-                break;
-                //case "subscriptions":
-                //    var subResponse = JsonConvert.DeserializeObject<SubscriptionsResponse>(message);
-                //    if (subResponse is not null)
-                //    {
-                //        foreach (var a in subResponse.Events)
-                //        {
-                //            if (a.Subscriptions.User is not null)
-                //                Console.WriteLine("User: " + JsonConvert.SerializeObject(a.Subscriptions.User));
-                //            if (a.Subscriptions.Level2 is not null)
-                //                Console.WriteLine("Level2: " + JsonConvert.SerializeObject(a.Subscriptions.Level2));
-                //            if (a.Subscriptions.Ticker is not null)
-                //                Console.WriteLine("Ticker: " + JsonConvert.SerializeObject(a.Subscriptions.Ticker));
-                //            if (a.Subscriptions.TickerBatch is not null)
-                //                Console.WriteLine("TickerBatch: " + JsonConvert.SerializeObject(a.Subscriptions.TickerBatch));
-                //        }
-                //    }
-                //    break;
-                //case "heartbeats":
-                //    var hbResp = JsonConvert.DeserializeObject<HeartbeatsStreamResponse>(message);
-                //    break;
-        }
-    }
+    Console.WriteLine(message);
 }
 
-string GenerateMessage(string key, string secret, IEnumerable<string> productIds, string channelName, long unixTimeStamp)
+static string GenerateMessage(string key, string secret, IEnumerable<string> productIds, string channelName, long unixTimeStamp)
 {
     var str = JsonConvert.SerializeObject(new
     {
